4b4986cae20cc1634c511e48a2ab5ffde48fafa3
Vizitys <53524661+Vizitys@users.noreply.github.com>
2022-10-15 21:57:05 +0300

    added a simple animation

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,34 @@
 <script lang="ts">
+  import Graphemer from "graphemer";
+
+  // dumb solution for dumb unicode problem aka emojis being more than one character
+  const graphemer = new Graphemer();
+
+  const nameGoal = "Vinski Lång";
+  const descriptionGoal = "Site still in developement 🛠️";
+
+  let name = "";
+  let description = "";
+
+  graphemer.splitGraphemes(nameGoal).forEach((char, i) => {
+    setTimeout(() => {
+      name += char;
+    }, 100 * i);
+  });
+
+  setTimeout(() => {
+    graphemer.splitGraphemes(descriptionGoal).forEach((char, i) => {
+      setTimeout(() => {
+        description += char;
+      }, 100 * i);
+    });
+  }, 100 * graphemer.countGraphemes(name) + 2000);
 </script>
 
 <main>
   <div class="content">
-    <h1 class="me">Vinski Lång</h1>
-    <p class="description">Site still in developement 🛠️</p>
+    <h1 class="me">{name}</h1>
+    <p class="description">{description}</p>
   </div>
 </main>
 
@@ -19,5 +43,23 @@
 
   .content {
     text-align: center;
+  }
+
+  @keyframes blink {
+    0% {
+      opacity: 0;
+    }
+    50% {
+      opacity: 1;
+    }
+    100% {
+      opacity: 0;
+    }
+  }
+
+  .description::after {
+    opacity: 0;
+    content: "|";
+    animation: blink 1s 1.5s step-end infinite;
   }
 </style>
